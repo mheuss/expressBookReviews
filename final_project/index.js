7c2a66977b30009aec6b7b261ab2a21eb780814b
Michael Heuss <mrheuss@gmail.com>
2024-06-19 20:56:13 -0400

    Up to task 2

--- final_project/index.js
+++ final_project/index.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
+const books_routes = require('./router/booksdb.js');
 
 const app = express();
 
@@ -10,11 +11,25 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
+app.use("/books", books_routes);
+
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    if (req.session.authorization) {
+        token = req.session.authorization["accessToken"];
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user;
+                next();
+            } else {
+                return res.status(403).json({ message: "User not authenticated" });
+            }
+        });
+    } else {
+        return res.status(403).json({ message: "User not logged in" });
+    }
 });
  
-const PORT =5000;
+const PORT =8012;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,46 @@
+const express = require("express");
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
-}
+  1: {
+    author: "Chinua Achebe",
+    title: "Things Fall Apart",
+    reviews: {},
+  },
+  2: { author: "Hans Christian Andersen", title: "Fairy tales", reviews: {} },
+  3: { author: "Dante Alighieri", title: "The Divine Comedy", reviews: {} },
+  4: { author: "Unknown", title: "The Epic Of Gilgamesh", reviews: {} },
+  5: { author: "Unknown", title: "The Book Of Job", reviews: {} },
+  6: { author: "Unknown", title: "One Thousand and One Nights", reviews: {} },
+  7: { author: "Unknown", title: "Nj\u00e1l's Saga", reviews: {} },
+  8: { author: "Jane Austen", title: "Pride and Prejudice", reviews: {} },
+  9: {
+    author: "Honor\u00e9 de Balzac",
+    title: "Le P\u00e8re Goriot",
+    reviews: {},
+  },
+  10: {
+    author: "Samuel Beckett",
+    title: "Molloy, Malone Dies, The Unnamable, the trilogy",
+    reviews: {},
+  },
+};
 
-module.exports=books;
+const books_routes = express.Router();
+
+books_routes.get("/", (req, res) => {
+  // Update the code here
+  res.json(books);
+});
+
+books_routes.get("/isbn/:isbn", (req, res) => {
+  // Get param
+  const isbn = req.params.isbn;
+  // Check if the book exists
+  if (books[isbn]) {
+    res.json(books[isbn]);
+  } else {
+    // Bail, baby!
+    res.status(404).send("Book not found");
+  }
+});
+
+module.exports = books_routes;
